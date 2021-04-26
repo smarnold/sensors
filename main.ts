8093cb860ec4e734531b412368d3d1da42893929
Stephen Arnold <smarnold@me.com>
2021-04-26 15:13:00 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     sensor = 2
 })
+let degrees = 0
 let sensor = 0
 sensor = 0
 basic.forever(function () {
@@ -23,7 +24,18 @@ basic.forever(function () {
         basic.showString("" + (input.temperature()))
         music.playTone(input.temperature(), music.beat(BeatFraction.Eighth))
     } else {
-        basic.showString("" + (input.compassHeading()))
+        degrees = input.compassHeading()
+        if (degrees < 3 || degrees > 357) {
+            basic.showString("N")
+        } else if (degrees < 93 && degrees > 87) {
+            basic.showString("E")
+        } else if (degrees < 183 && degrees > 177) {
+            basic.showString("S")
+        } else if (degrees < 273 && degrees > 267) {
+            basic.showString("W")
+        } else {
+            basic.showString("" + (degrees))
+        }
         music.playTone(input.compassHeading(), music.beat(BeatFraction.Eighth))
     }
 })
